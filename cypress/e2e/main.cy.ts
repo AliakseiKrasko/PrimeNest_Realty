87bf75483dde638da7a439d59d0fb020f03c242e
Aliaksei Krasko <alekskrasko1985@gmail.com>
2025-08-05 11:21:34 +0300

    create type and mocks for estate3

--- cypress/e2e/main.cy.ts
+++ cypress/e2e/main.cy.ts
@@ -1,6 +1,6 @@
 describe('Главная страница', () => {
     it('открывается и содержит Welcome', () => {
         cy.visit('http://localhost:3000');
-        cy.contains('Tailwind работает!');
+        cy.contains('Каталог недвижимости');
     });
 });
