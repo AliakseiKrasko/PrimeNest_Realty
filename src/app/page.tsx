80f2541ae49ea1254fbe8fe9dbff1edb8a4876e4
Aliaksei Krasko <alekskrasko1985@gmail.com>
2025-08-05 11:47:13 +0300

    create i18n

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,23 @@
+"use client"
+
+
+import {useTranslation} from "react-i18next";
 import {ESTATES} from "@/mocks/estates";
 import EstateCard from "@/components/EstateCard";
+import i18n from "i18next";
 
 export default function HomePage() {
+    const { t } = useTranslation();
+
     return (
         <main className="max-w-5xl mx-auto py-10">
-            <h1 className="text-3xl font-bolt mb-6">Каталог недвижимости</h1>
+            <h1 className="text-3xl font-bolt mb-6">{t("catalog")}</h1>
             <div>
                 {ESTATES.map(estate => (
                     <EstateCard key={estate.id} estate={estate} />
                 ))}
+                <button onClick={() => i18n.changeLanguage("en")}>EN</button>
+                <button onClick={() => i18n.changeLanguage("ru")}>RU</button>
             </div>
         </main>
     );
--- src/components/EstateCard.tsx
+++ src/components/EstateCard.tsx
@@ -1,10 +1,19 @@
+"use client"
+
+import "@/i18n";
 import {Estate} from "@/shared/types/estate";
+import {useTranslation} from "react-i18next";
 
 interface EstateCard {
     estate: Estate;
 }
 
 export default function EstateCard({estate}: EstateCard) {
+    const { t } = useTranslation();
+
+    // Ключ для типа объекта
+    const typeKey = `type_${estate.type}`;
+
     return (
         <div className="rounded-2xl overflow-hidden shadow-lg bg-white flex flex-col">
             <img
@@ -16,10 +25,13 @@ export default function EstateCard({estate}: EstateCard) {
                 <h2 className="text-xl font-semibold mb-2">{estate.title}</h2>
                 <div className="text-gray-600 mb-1">{estate.city}</div>
                 <div className="text-gray-700 mb-2">{estate.address}</div>
-                <div className="text-base font-bolt mb-2">{estate.price.toLocaleString()}</div>
+                <div className="text-base font-bolt mb-2">
+                    {t('price')}: {estate.price.toLocaleString()}
+                </div>
                 <div className="text-sm text-gray-500">
-                    {estate.type === "apartment" ? "Квартира" : estate.type === "house" ? "Дом" : "Комерческая"}
-                    &nbsp;|&nbsp;{estate.rooms} комн. | {estate.area} м²
+                    {t(typeKey)}&nbsp;|&nbsp;
+                    {estate.rooms} {t('rooms')}&nbsp;|&nbsp;
+                    {estate.area} {t('area')}
                 </div>
             </div>
         </div>
