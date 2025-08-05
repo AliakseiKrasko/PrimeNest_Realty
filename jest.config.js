3c4c43c39dc7a513de7c8e4204dab094498ea711
Aliaksei Krasko <alekskrasko1985@gmail.com>
2025-08-05 08:52:06 +0300

    trigger workflow3

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,17 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
     setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/src/$1',
-        '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+        '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
     },
     transform: {
         '^.+\\.(ts|tsx)$': 'ts-jest'
+    },
+    globals: {
+        'ts-jest': {
+            tsconfig: 'tsconfig.jest.json'
+        }
     }
 };
