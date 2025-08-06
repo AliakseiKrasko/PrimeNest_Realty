3a1e7f9ea9d48d80f6505376d5b9f3356f744388
Aliaksei Krasko <alekskrasko1985@gmail.com>
2025-08-06 09:28:28 +0300

    create stare

---
+++ src/store/store.ts
@@ -0,0 +1,13 @@
+import {configureStore} from "@reduxjs/toolkit";
+
+export const store = configureStore({
+    reducer: {
+        [estatesApi.reducerPath]: estatesApi.reducer,
+        filters: filtersReducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(estatesApi.middleware)
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = ReturnType<typeof store.dispatch>
